77d522f975ecdec5e87ec5b40833971fe553d1ed
brenonun3s <morais.brenonunes@hotmail.com>
2025-08-21 09:55:48 -0300

    Alterado o mapeamento do ProdutoMapper para corrigir bug do Post e Put de Produtos - Corrigido e OK

--- src/main/java/br/com/estoque/mapper/ProdutoMapper.java
+++ src/main/java/br/com/estoque/mapper/ProdutoMapper.java
@@ -13,14 +13,12 @@ import br.com.estoque.model.Produto;
 public interface ProdutoMapper {
 
     @Mapping(target = "estoques", ignore = true) 
-    @Mapping(target = "categoria", ignore = true)
     @Mapping(target = "id", ignore = true)
     Produto toEntity(ProdutoRequestDTO dto);
 
     ProdutoResponseDTO toResponseDTO(Produto produto);
 
     @Mapping(target = "estoques", ignore = true) 
-    @Mapping(target = "categoria", ignore = true)
     @Mapping(target = "id", ignore = true)
     void updateProdutoFromDto(ProdutoUpdateDTO dto, @MappingTarget Produto produto);
 }
