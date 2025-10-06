231dc700234d65db66455f18da90e934f2e3b9fb
Breno Nunes <morais.brenonunes@hotmail.com>
2025-10-06 19:18:45 -0300

    fix(WebConfig): alterado para o backend correto

--- src/main/java/br/com/estoque/config/WebConfig.java
+++ src/main/java/br/com/estoque/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") 
-                        .allowedOrigins("https://frontend-estoque360.vercel.app/")
+                        .allowedOrigins("https://gestor-estoque.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
