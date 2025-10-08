7cc4d4b21e485455aca5150d4a283c7cfe725714
Breno Nunes <morais.brenonunes@hotmail.com>
2025-10-08 06:57:45 -0300

    fix(SecurityConfig): liberado o frontend de produção

--- src/main/java/br/com/estoque/config/security/SecurityConfig.java
+++ src/main/java/br/com/estoque/config/security/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     http
     .cors(cors -> cors.configurationSource(request -> {
       var config = new org.springframework.web.cors.CorsConfiguration();
-      config.setAllowedOrigins(List.of("http://localhost:5173"));
+      config.setAllowedOrigins(List.of("http://localhost:5173", "https://frontend-estoque360.vercel.app"));
       config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
       config.setAllowedHeaders(List.of("*"));
       config.setAllowCredentials(true);
