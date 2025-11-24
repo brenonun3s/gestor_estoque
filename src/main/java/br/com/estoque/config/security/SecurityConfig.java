06c0af529a00d8188d49527f9b9d6e114f5d471e
Breno Nunes <morais.brenonunes@hotmail.com>
2025-11-24 14:07:58 -0300

    fix: efetuado alterações para aplicação ser preparada para produção. Bug pendente de correção: requisições do frontend está falhando

--- src/main/java/br/com/estoque/config/security/SecurityConfig.java
+++ src/main/java/br/com/estoque/config/security/SecurityConfig.java
@@ -1,59 +1,68 @@
-  package br.com.estoque.config.security;
+package br.com.estoque.config.security;
 
-  import java.util.List;
+import java.util.List;
 
-  import org.springframework.context.annotation.Bean;
-  import org.springframework.context.annotation.Configuration;
-  import org.springframework.security.authentication.AuthenticationManager;
-  import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-  import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-  import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-  import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-  import org.springframework.security.config.http.SessionCreationPolicy;
-  import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-  import org.springframework.security.crypto.password.PasswordEncoder;
-  import org.springframework.security.web.SecurityFilterChain;
-  import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-  @Configuration
-  @EnableWebSecurity
-  public class SecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
-      http
-      .cors(cors -> cors.configurationSource(request -> {
-        var config = new org.springframework.web.cors.CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://frontend-estoque360.vercel.app"));
-        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(true);
-        return config;
-    }))
-      .csrf(AbstractHttpConfigurer::disable)
-          .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
-          .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-          .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/auth/**", "/api/cnpj/**","/api/cep/**")
-                            .permitAll()
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http, JwtFilter jwtFilter) throws Exception {
+    http
+        .cors().configurationSource(corsConfigurationSource())
+        .and()
+        .csrf(AbstractHttpConfigurer::disable)
+        .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
+        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/auth/**", "/api/cnpj/**", "/api/cep/**").permitAll()
+            .anyRequest().authenticated())
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
-              .anyRequest().authenticated()
-
-          )
-          .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-          
-      return http.build();
-
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-      return config.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-      return new BCryptPasswordEncoder();
-    }
-
+    return http.build();
   }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    return config.getAuthenticationManager();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(List.of(
+        "http://localhost:5173",
+        "https://frontend-estoque360.vercel.app",
+        "https://lacie-hyperdulic-stacee.ngrok-free.dev"
+      ));
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("*"));
+    config.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+    return source;
+  }
+
+}
